7190e3a806a16865dbeb62157fb5d48fa741a64a
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-09-20 16:41:19 -0400

    Add files via upload

---
+++ week2problem1.java
@@ -0,0 +1,25 @@
+package cs176;
+
+public class week2problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Problem 1
+		
+		System.out.println("\\(>_<)");
+		System.out.println("  ))z");
+		System.out.println("  / \\");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
