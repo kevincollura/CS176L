02ba67d46d27ec3e37eca971fcac8030dc83a583
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-09-20 21:00:08 -0400

    Add files via upload

---
+++ week3pr1.java
@@ -0,0 +1,24 @@
+package cs176;
+
+public class week3pr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee Sam = new Employee ("Sam", 70000);
+		System.out.println(Sam.getName());
+		System.out.println(Sam.getSalary());
+		Sam.raiseSalary(10); // raise by 10%
+		System.out.println(Sam.getSalary());
+		
+		
+		
+		
+
+		
+		
+		
+		
+	}
+
+}
