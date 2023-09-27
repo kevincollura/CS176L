61df2ec6f86f94b3fff1abff798ed2275c48eb16
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-09-27 17:00:17 -0400

    Add files via upload

---
+++ Week3prob2.java
@@ -0,0 +1,25 @@
+package cs176;
+
+public class Week3prob2 {
+
+	
+	public static void main(String[] args) {
+			BankAccount Kevin = new BankAccount("Kevin", 0);
+			System.out.println("Account Balance: " + Kevin.balance());
+			System.out.println("Amount After Deposit: " + Kevin.deposit(12000));
+			System.out.println("Amount After Withdraw: " + Kevin.withdraw(500));
+					
+				}
+
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
