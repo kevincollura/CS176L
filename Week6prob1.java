5431913ed896075a0835a562abf81ebea8ac0206
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-10-11 19:57:44 -0400

    Add files via upload

---
+++ Week6prob1.java
@@ -0,0 +1,23 @@
+package cs176;
+import java.util.Scanner;
+
+public class Week6prob1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        while (true) {
+            System.out.println(name);
+        }
+	          
+		
+		
+	}
+
+}
+
